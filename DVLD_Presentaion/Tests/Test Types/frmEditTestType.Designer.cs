feacd0c9c4d5de2c33d87c69187d407f2db1eaca
Amjad <amjadjaloom720@gmail.com>
2026-05-04 22:51:17 +0300

    Update Manage Test Types: Edit Test Type

--- DVLD_Presentaion/Tests/Test Types/frmEditTestType.Designer.cs
+++ DVLD_Presentaion/Tests/Test Types/frmEditTestType.Designer.cs
@@ -46,9 +46,10 @@
             // btnCancel
             // 
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btnCancel.Location = new System.Drawing.Point(408, 416);
+            this.btnCancel.Location = new System.Drawing.Point(272, 270);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(133, 59);
+            this.btnCancel.Size = new System.Drawing.Size(89, 38);
             this.btnCancel.TabIndex = 4;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -57,9 +58,10 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btnSave.Location = new System.Drawing.Point(547, 416);
+            this.btnSave.Location = new System.Drawing.Point(365, 270);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(133, 59);
+            this.btnSave.Size = new System.Drawing.Size(89, 38);
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -68,28 +70,31 @@
             // tbTestFees
             // 
             this.tbTestFees.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.tbTestFees.Location = new System.Drawing.Point(12, 363);
+            this.tbTestFees.Location = new System.Drawing.Point(8, 236);
+            this.tbTestFees.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbTestFees.Name = "tbTestFees";
-            this.tbTestFees.Size = new System.Drawing.Size(668, 35);
+            this.tbTestFees.Size = new System.Drawing.Size(447, 26);
             this.tbTestFees.TabIndex = 2;
-            this.tbTestFees.Leave += new System.EventHandler(this.tbTestFees_Leave);
+            this.tbTestFees.Validating += new System.ComponentModel.CancelEventHandler(this.tbTestFees_Validating);
             // 
             // tbTestTitle
             // 
             this.tbTestTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.tbTestTitle.Location = new System.Drawing.Point(12, 49);
+            this.tbTestTitle.Location = new System.Drawing.Point(8, 32);
+            this.tbTestTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbTestTitle.Name = "tbTestTitle";
-            this.tbTestTitle.Size = new System.Drawing.Size(668, 35);
+            this.tbTestTitle.Size = new System.Drawing.Size(447, 26);
             this.tbTestTitle.TabIndex = 0;
-            this.tbTestTitle.Leave += new System.EventHandler(this.tbTestTitle_Leave);
+            this.tbTestTitle.Validating += new System.ComponentModel.CancelEventHandler(this.tbTestTitle_Validating);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label4.Location = new System.Drawing.Point(12, 331);
+            this.label4.Location = new System.Drawing.Point(8, 215);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(74, 29);
+            this.label4.Size = new System.Drawing.Size(49, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "Fees:";
             // 
@@ -97,9 +102,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label3.Location = new System.Drawing.Point(12, 17);
+            this.label3.Location = new System.Drawing.Point(8, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 29);
+            this.label3.Size = new System.Drawing.Size(42, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "Title:";
             // 
@@ -107,9 +113,10 @@
             // 
             this.lblTestID.AutoSize = true;
             this.lblTestID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.lblTestID.Location = new System.Drawing.Point(628, 17);
+            this.lblTestID.Location = new System.Drawing.Point(419, 11);
+            this.lblTestID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTestID.Name = "lblTestID";
-            this.lblTestID.Size = new System.Drawing.Size(52, 29);
+            this.lblTestID.Size = new System.Drawing.Size(36, 20);
             this.lblTestID.TabIndex = 9;
             this.lblTestID.Text = "###";
             // 
@@ -117,29 +124,32 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label1.Location = new System.Drawing.Point(580, 17);
+            this.label1.Location = new System.Drawing.Point(387, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 29);
+            this.label1.Size = new System.Drawing.Size(30, 20);
             this.label1.TabIndex = 8;
             this.label1.Text = "ID:";
             // 
             // tbTestDesc
             // 
             this.tbTestDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.tbTestDesc.Location = new System.Drawing.Point(12, 132);
+            this.tbTestDesc.Location = new System.Drawing.Point(8, 86);
+            this.tbTestDesc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbTestDesc.Multiline = true;
             this.tbTestDesc.Name = "tbTestDesc";
-            this.tbTestDesc.Size = new System.Drawing.Size(668, 181);
+            this.tbTestDesc.Size = new System.Drawing.Size(447, 119);
             this.tbTestDesc.TabIndex = 1;
-            this.tbTestDesc.Leave += new System.EventHandler(this.tbTestDesc_Leave);
+            this.tbTestDesc.Validating += new System.ComponentModel.CancelEventHandler(this.tbTestDesc_Validating);
             // 
             // lblDesc
             // 
             this.lblDesc.AutoSize = true;
             this.lblDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.lblDesc.Location = new System.Drawing.Point(12, 100);
+            this.lblDesc.Location = new System.Drawing.Point(8, 65);
+            this.lblDesc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDesc.Name = "lblDesc";
-            this.lblDesc.Size = new System.Drawing.Size(74, 29);
+            this.lblDesc.Size = new System.Drawing.Size(50, 20);
             this.lblDesc.TabIndex = 6;
             this.lblDesc.Text = "Desc:";
             // 
@@ -147,11 +157,11 @@
             // 
             this.errorProvider1.ContainerControl = this;
             // 
-            // frmUpdateTestType
+            // frmEditTestType
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 487);
+            this.ClientSize = new System.Drawing.Size(461, 317);
             this.Controls.Add(this.lblDesc);
             this.Controls.Add(this.tbTestDesc);
             this.Controls.Add(this.btnCancel);
@@ -162,7 +172,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lblTestID);
             this.Controls.Add(this.label1);
-            this.Name = "frmUpdateTestType";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmEditTestType";
             this.Text = "frmUpdateTestType";
             this.Load += new System.EventHandler(this.frmUpdateTestType_Load);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
--- DVLD_Presentaion/Tests/Test Types/frmEditTestType.cs
+++ DVLD_Presentaion/Tests/Test Types/frmEditTestType.cs
@@ -7,82 +7,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _19___Project___DVLD.Global_Classes;
 using DVLD_Business;
 
 namespace _19___Project___DVLD.Test_Types
 {
     public partial class frmEditTestType : Form
     {
-        clsTestType testType = null;
-        public frmEditTestType(clsTestType testType)
+        clsTestType _TestType;
+        private clsTestType.enTestType _TestTypeID = clsTestType.enTestType.VisionTest;
+        public frmEditTestType(clsTestType.enTestType testTypeID)
         {
             InitializeComponent();
-            this.testType = testType;
+            _TestTypeID = testTypeID;
         }
         private void frmUpdateTestType_Load(object sender, EventArgs e)
         {
-
-            LoadTestTypes();
-        }
-        private void LoadTestTypes()
-        {
-            if (testType != null)
+            _TestType = clsTestType.Find(_TestTypeID);
+            if (_TestType != null)
             {
-                lblTestID.Text = testType.ID.ToString();
-                tbTestTitle.Text = testType.Title;
-                tbTestDesc.Text = testType.Description;
-                tbTestFees.Text = testType.Fees.ToString();
+                lblTestID.Text = _TestType.ID.ToString();
+                tbTestTitle.Text = _TestType.Title.ToString();
+                tbTestDesc.Text = _TestType.Description.ToString();
+                tbTestFees.Text = _TestType.Fees.ToString();
             }
             else
             {
-                MessageBox.Show("Test object is null to load!");
+                MessageBox.Show("Could not find Test Type with id = " + _TestTypeID.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
             }
         }
         private void btnSave_Click(object sender, EventArgs e)
         {
 
-            if (AreFieldsValied())
+            if (!ValidateChildren())
             {
-                if (UpdateTestType())
-                {
-                    MessageBox.Show("Upated successfully!");
-                }
-                else
-                    MessageBox.Show("Not Upated!");
+                MessageBox.Show("Some fileds are not valide!, put the mouse over the red icon(s) to see the erro", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
-        }
-        private bool AreFieldsValied()
-        {
-            if (
-                !clsCommonMethods.HasErrors(errorProvider1.GetError(tbTestTitle)) &&
-                !clsCommonMethods.HasErrors(errorProvider1.GetError(tbTestDesc)) &&
-                !clsCommonMethods.HasErrors(errorProvider1.GetError(tbTestFees))
-                )
+
+            _TestType.Title = tbTestTitle.Text.Trim();
+            _TestType.Description = tbTestDesc.Text.Trim();
+            _TestType.Fees = Convert.ToSingle(tbTestFees.Text.Trim());
+
+            if (_TestType.Save())
             {
-                return true;
+                MessageBox.Show("Test Type updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
             }
-            return false;
-        }
-        private bool UpdateTestType()
-        {
-            clsTestType test = new clsTestType(Convert.ToInt32(lblTestID.Text), tbTestTitle.Text, tbTestDesc.Text, (float)Convert.ToDouble(tbTestFees.Text));
-            return clsTestType.UpdateTestType(test);
-        }
-        private void tbTestTitle_Leave(object sender, EventArgs e)
-        {
-            clsCommonMethods.MakeTextBoxFieldRequired(tbTestTitle, errorProvider1);
-        }
-        private void tbTestDesc_Leave(object sender, EventArgs e)
-        {
-            clsCommonMethods.MakeTextBoxFieldRequired(tbTestDesc, errorProvider1);
-        }
-        private void tbTestFees_Leave(object sender, EventArgs e)
-        {
-            clsCommonMethods.MakeTextBoxFieldRequired(tbTestFees, errorProvider1);
+            else
+            {
+                MessageBox.Show("Failed to update Test Type!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
         private void btnCancel_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void tbTestTitle_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbTestTitle.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(tbTestTitle, "Title is required!");
+            }
+            else
+                errorProvider1.SetError(tbTestTitle, string.Empty);
+        }
+
+        private void tbTestDesc_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbTestTitle.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(tbTestDesc, "Description is required!");
+            }
+            else
+                errorProvider1.SetError(tbTestDesc, string.Empty);
+        }
+
+        private void tbTestFees_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbTestFees.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(tbTestFees, "Fees is required!");
+            }
+            else
+            {
+                if (!clsValidation.IsNumber(tbTestFees.Text))
+                {
+                    e.Cancel = true;
+                    errorProvider1.SetError(tbTestFees, "Fees must be a number!");
+                }
+                else
+                    errorProvider1.SetError(tbTestFees, string.Empty);
+            }
         }
     }
 }
