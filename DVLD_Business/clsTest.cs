f7b148d1835274bb1d0908ef65ea0f467e8705f7
Amjad <amjadjaloom720@gmail.com>
2026-05-20 17:19:03 +0300

    Update Test Business & Data Access

--- DVLD_Business/clsTest.cs
+++ DVLD_Business/clsTest.cs
@@ -1,29 +1,150 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DVLD_DataAccess;
 
 namespace DVLD_Business
 {
     public class clsTest
     {
-        public bool TestResult { get; set; }
-        public clsTestAppointment TestAppointmentInfo { get; set; }
 
-        internal static clsTest FindLastTestPerPersonAndLicenseClass(int applicantPersonID, int licenseClassID, clsTestType.enTestType testTypeID)
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int TestID { set; get; }
+        public int TestAppointmentID { set; get; }
+        public clsTestAppointment TestAppointmentInfo { set; get; }
+        public bool TestResult { set; get; }
+        public string Notes { set; get; }
+        public int CreatedByUserID { set; get; }
+
+        public clsTest()
+
         {
-            throw new NotImplementedException();
+            this.TestID = -1;
+            this.TestAppointmentID = -1;
+            this.TestResult = false;
+            this.Notes = "";
+            this.CreatedByUserID = -1;
+
+            Mode = enMode.AddNew;
+
         }
 
-        internal static byte GetPassedTestCount(int localDrivingLicenseApplicationID)
+        public clsTest(int TestID, int TestAppointmentID,
+            bool TestResult, string Notes, int CreatedByUserID)
+
         {
-            throw new NotImplementedException();
+            this.TestID = TestID;
+            this.TestAppointmentID = TestAppointmentID;
+            this.TestAppointmentInfo = clsTestAppointment.Find(TestAppointmentID);
+            this.TestResult = TestResult;
+            this.Notes = Notes;
+            this.CreatedByUserID = CreatedByUserID;
+
+            Mode = enMode.Update;
         }
 
-        internal static bool PassedAllTests(int localDrivingLicenseApplicationID)
+        private bool _AddNewTest()
         {
-            throw new NotImplementedException();
+            //call DataAccess Layer 
+
+            this.TestID = clsTestData.AddNewTest(this.TestAppointmentID,
+                this.TestResult, this.Notes, this.CreatedByUserID);
+
+
+            return (this.TestID != -1);
         }
+
+        private bool _UpdateTest()
+        {
+            //call DataAccess Layer 
+
+            return clsTestData.UpdateTest(this.TestID, this.TestAppointmentID,
+                this.TestResult, this.Notes, this.CreatedByUserID);
+        }
+
+        public static clsTest Find(int TestID)
+        {
+            int TestAppointmentID = -1;
+            bool TestResult = false; string Notes = ""; int CreatedByUserID = -1;
+
+            if (clsTestData.GetTestInfoByID(TestID,
+            ref TestAppointmentID, ref TestResult,
+            ref Notes, ref CreatedByUserID))
+
+                return new clsTest(TestID,
+                        TestAppointmentID, TestResult,
+                        Notes, CreatedByUserID);
+            else
+                return null;
+
+        }
+
+        public static clsTest FindLastTestPerPersonAndLicenseClass
+            (int PersonID, int LicenseClassID, clsTestType.enTestType TestTypeID)
+        {
+            int TestID = -1;
+            int TestAppointmentID = -1;
+            bool TestResult = false; string Notes = ""; int CreatedByUserID = -1;
+
+            if (clsTestData.GetLastTestByPersonAndTestTypeAndLicenseClass
+                (PersonID, LicenseClassID, (int)TestTypeID, ref TestID,
+            ref TestAppointmentID, ref TestResult,
+            ref Notes, ref CreatedByUserID))
+
+                return new clsTest(TestID,
+                        TestAppointmentID, TestResult,
+                        Notes, CreatedByUserID);
+            else
+                return null;
+
+        }
+
+        public static DataTable GetAllTests()
+        {
+            return clsTestData.GetAllTests();
+
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewTest())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateTest();
+
+            }
+
+            return false;
+        }
+
+        public static byte GetPassedTestCount(int LocalDrivingLicenseApplicationID)
+        {
+            return clsTestData.GetPassedTestCount(LocalDrivingLicenseApplicationID);
+        }
+
+        public static bool PassedAllTests(int LocalDrivingLicenseApplicationID)
+        {
+            //if total passed test less than 3 it will return false otherwise will return true
+            return GetPassedTestCount(LocalDrivingLicenseApplicationID) == 3;
+        }
+
     }
 }
